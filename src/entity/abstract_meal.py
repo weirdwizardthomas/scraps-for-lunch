e6cfaf5a53d3ec3f962adf709b296614c9b87380
weirdwizardtom <koristo1@fit.cvut.cz>
2022-04-08 21:42:02 +0200

    Add https://www.lemon.cz

--- src/entity/abstract_meal.py
+++ src/entity/abstract_meal.py
@@ -6,3 +6,6 @@ class AbstractMeal:
 
     def __str__(self):
         return f'{self.title}, {self.price}, {self.allergens}'
+
+    def __repr__(self):
+        return self.__str__()
--- src/sites/lemon_leaf.py
+++ src/sites/lemon_leaf.py
@@ -1,18 +1,22 @@
-import bs4
 import requests
-from bs4 import BeautifulSoup, NavigableString, Tag
+from bs4 import BeautifulSoup
 
+from src.entity.lemon_leaf.meal import Meal
 from src.entity.version import Version
-from src.util import remove_whitechars
 
-VERSION = Version('1.0', 2022, 3, 31)
+VERSION = Version('1.0', 2022, 4, 10)
 
 URL = 'https://www.lemon.cz/en/lunch-menu-2/'
 
-page = requests.get(URL)
-soup = BeautifulSoup(page.content, 'html.parser')
 
-current_element = soup.find('h2', class_='day-now')
+def get_menu():
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
 
-for x in (current_element.find_next_siblings('div', class_='lunch-food-item'))[:3]:
-    print(remove_whitechars(x.find('p').text))
+    current_element = soup.find('h2', class_='day-now')
+
+    menu_items = current_element.find_next_siblings('div', class_='lunch-food-item')[:3]
+
+    return {
+        'Všechna jídla': [Meal(item) for item in menu_items]
+    }
