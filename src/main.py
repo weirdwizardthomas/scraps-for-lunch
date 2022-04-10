8c93170aa08cce6878f5b71940179b9aba692061
weirdwizardtom <koristo1@fit.cvut.cz>
2022-04-10 19:21:37 +0200

    Extract menu printing

--- src/main.py
+++ src/main.py
@@ -1,21 +1,9 @@
 from src.sites import *
 
+
 if __name__ == '__main__':
-
-    print('Jina krajina')
-    for course, meals in jina_krajina.get_menu().items():
-        print(f'\t{course}')
-        for meal in meals:
-            print(f'\t\t{meal}')
-
-    print('Pricny rez')
-    for course, meals in pricny_rez.get_menu().items():
-        print(f'\t{course}')
-        for meal in meals:
-            print(f'\t\t{meal}')
-
-    print('Kathmandu')
-    for course, meals in kathmandu.get_menu().items():
-        print(f'\t{course}')
-        for meal in meals:
-            print(f'\t\t{meal}')
+    print_menu('Jina krajina', jina_krajina)
+    print_menu('Pricny rez', pricny_rez)
+    print_menu('Kathmandu', kathmandu)
+    print_menu('Lemon leaf', lemon_leaf)
+    print_menu('U cizku', u_cizku)
--- src/util.py
+++ src/util.py
@@ -4,3 +4,15 @@ def strike(text):
 
 def remove_whitechars(text):
     return ' '.join([c.strip() for c in text.split()])
+
+
+def print_menu(name, menu):
+    print(name)
+    try:
+        menu = menu.get_menu()
+        for course, meals in menu.items():
+            print(f'\t{course}')
+            for meal in meals:
+                print(f'\t\t{meal}')
+    except Exception as e:
+        print(f'\tFollowing error occurred getting the menu: {e}')
